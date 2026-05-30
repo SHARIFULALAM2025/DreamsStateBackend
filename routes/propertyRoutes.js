425768d5ee723acf4c6d060ccdf1845ca59f1b86
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-05-30 18:27:24 +0600

    update

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -14,10 +14,9 @@ router.post('/add', addProperty)
 // ২. সাধারণ গেট রাউট (সব প্রপার্টি)
 router.get('/', getProperties)
 
-// ৩. সুনির্দিষ্ট রাউট (এটি অবশ্যই /:id এর উপরে থাকবে)
 router.get('/buy', getBuyProperties) // <--- এখানে নিয়ে আসুন
 
-// ৪. ডাইনামিক রাউট (সবার নিচে)
+
 router.get('/:id', getPropertyById)
 
 module.exports = router
